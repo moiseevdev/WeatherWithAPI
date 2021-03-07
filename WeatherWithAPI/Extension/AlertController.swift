a30ff04b6ea4731961ee1c9b7840920a8d75ee9a
Moiseev Andrei <andreimoiseew2009@yandex.ru>
2021-03-07 23:59:57 +0300

    add AlertConroller

---
+++ WeatherWithAPI/Extension/AlertController.swift
@@ -0,0 +1,8 @@
+//
+//  AlertController.swift
+//  WeatherWithAPI
+//
+//  Created by Андрей Моисеев on 07.03.2021.
+//
+
+import Foundation
---
+++ WeatherWithAPI/Supporting files/Const.swift
@@ -0,0 +1,10 @@
+//
+//  Const.swift
+//  WeatherWithAPI
+//
+//  Created by Андрей Моисеев on 07.03.2021.
+//
+
+import Foundation
+
+//let apiKey = 105a9207bffb3ade01dc2c893db322a7
